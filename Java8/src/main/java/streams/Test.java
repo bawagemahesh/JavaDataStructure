84a7581b42365a3692238ac34d4af4f15ac4beee
mahesh bawage <bawage.mahesh7@gmail.com>
2020-12-28 21:46:41 +0530

    Added files

---
+++ Java8/src/main/java/streams/Test.java
@@ -0,0 +1,30 @@
+package streams;
+
+import java.time.Clock;
+import java.util.stream.Stream;
+
+class Main{
+    protected int x, y;
+
+}
+public class Test {
+    int a =10;
+    static int b=20;
+    public static void main(String[] args) {
+//        System.out.println(Clock.systemUTC());
+//        int a =30;
+//        Main m = new Main();
+//        Test s= new Test();
+//        s.b =40;
+//        System.out.println(a+" "+s.a+" "+b+" "+s.b);
+//        for (int i = 0;true ; i++) {
+//            System.out.println("Hello");
+//            continue;
+//            break;
+//        }
+
+        String[] ln={"dd","ddd","eeee"};
+        Stream num= Stream.of(ln);
+    }
+
+}
